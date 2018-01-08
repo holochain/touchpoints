c0c289d6b4caa2d2dd080f2114394c6f0630cf6e
mcscope <mcscope@gmail.com>
2018-01-07 16:09:04 -0800

    repair damage done by malicious text editors

--- dna/nfcTag/nfcTag.js
+++ dna/nfcTag/nfcTag.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function nfcTagCreate(tagId){
-    var key = commit("nfcTag",tagId);         // Commits a new nfcTag to my source chain
+    // Commits a new nfcTag to my source chain
+    var key = commit("nfcTag",tagId);
     return key;
 }
 
@@ -41,7 +42,7 @@ function interfaceUpdate(params) {
   return key;
 }
 function interfaceDelete() {
-  
+
 }
 
 
@@ -49,13 +50,19 @@ function getMe() {return App.Key.Hash;}
 
 function showAgentHistory(params) {
   var links = getLinks(params.agentId, "bump", {Load:true})
+  links.sort(bumpCmp);
   return links;
 }
 
 function showTagHistory(params) {
   var tagHash = makeHash("nfcTag", { id: params.tagId });
   var links = getLinks(tagHash, "bump", {Load:true});
+  links.sort(bumpCmp);
   return links;
+}
+
+function bumpCmp(a,b) {
+    return a.Entry.stamp < b.Entry.stamp;
 }
 
 // -----------------------------------------------------------------
