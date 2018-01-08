1bce766b96a5d031c56cbbe3d527606285f0910e
Jonathan Haber <jryanhaber@gmail.com>
2018-01-07 19:01:06 -0800

    extract icon button

--- app/src/IconButton.js
+++ app/src/IconButton.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 export default class IconButton extends Component {
   render(props) {
-    return <button className="icon">{this.props.label}</button>;
+    return (
+      <button className="icon">
+        <img className="iconImage" src={this.props._icon} />
+        <span className="icon-label">{this.props.label}</span>
+      </button>
+    );
   }
 }
