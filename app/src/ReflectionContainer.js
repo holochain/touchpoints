f650e9ef4d6dbf90673de970e7a4c4bdcd4c7582
Jonathan Haber <jryanhaber@gmail.com>
2018-01-07 18:13:38 -0800

    Create reflection page

---
+++ app/src/ReflectionContainer.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import Reflection from './Reflection';
+
+const mapStateToProps = state => {};
+
+const mapDispatchToProps = dispatch => {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Reflection);
